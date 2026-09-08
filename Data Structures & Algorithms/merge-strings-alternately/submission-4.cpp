dea0daa86a9dc4aa9142f859403a7e64e26a26cd
Govind V <govind498498@gmail.com>
2026-09-08 17:57:06 +0530

    Add: merge-strings-alternately - submission-4

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        string result;
+        result.reserve(n + m); 
+        
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            if (i < n) result += word1[i++];
+            if (j < m) result += word2[j++];
+        }
+        
+        return result;
+    }
+};
